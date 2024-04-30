b57d8c2489ad9cc41c2211625e75fa077af7340e
Your Name <142696188+PabloGarciaU@users.noreply.github.com>
2024-04-29 23:50:05 -0400

    probando rag multiple local
    con mi curriculum y el de german

--- utemia_backend.py
+++ utemia_backend.py
@@ -1,4 +1,3 @@
-#1. Import OS, Document Loader, Text Splitter, Bedrock Embeddings, Vector DB, VectorStoreIndex, Bedrock-LLM
 import os
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -7,37 +6,46 @@ from langchain_community.vectorstores import FAISS
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.llms.bedrock import Bedrock
 
-#5c. Wrap within a function
 def utemia_index():
-    #2. Define the data source and load data with PDFLoader
-    data_load=PyPDFLoader('https://vrac.utem.cl/wp-content/uploads/2021/05/reglamento-general-de-los-estudiantes-de-pregrado.pdf')
-    #3. Split the Text based on Character, Tokens etc. - Recursively split by character - ["\n\n", "\n", " ", ""]
-    data_split=RecursiveCharacterTextSplitter(separators=["\n\n", "\n", " ", ""], chunk_size=100,chunk_overlap=10)
-    #4. Create Embeddings -- Client connection
-    data_embeddings=BedrockEmbeddings(
-    credentials_profile_name= 'default',
-    model_id='cohere.embed-multilingual-v3')
-    #5à Create Vector DB, Store Embeddings and Index for Search - VectorstoreIndexCreator
-    data_index=VectorstoreIndexCreator(
+    docs_dir = 'docs'
+    loaders = []
+
+    for file_name in os.listdir(docs_dir):
+        if file_name.endswith('.pdf'):
+            file_path = os.path.join(docs_dir, file_name)
+            print(f"Cargando archivo: {file_path}")
+            loader = PyPDFLoader(file_path)
+            loaders.append(loader)
+
+    data_split = RecursiveCharacterTextSplitter( 
+        chunk_size=800,
+        chunk_overlap=100,
+        separators=["\n\n", "\n", "\n\n\n", "\n\n\n\n", "\n\n\n\n\n", "\n\n\n\n\n\n", "\n\n\n\n\n\n\n", "\n\n\n\n\n\n\n\n"]
+        )
+    
+    data_embeddings = BedrockEmbeddings(
+        credentials_profile_name='default',
+        model_id='cohere.embed-multilingual-v3')
+
+    data_index = VectorstoreIndexCreator(
         text_splitter=data_split,
         embedding=data_embeddings,
         vectorstore_cls=FAISS)
-    #5b  Create index for HR Policy Document
-    db_index=data_index.from_loaders([data_load])
+
+    db_index = data_index.from_loaders(loaders)
     return db_index
-#6a. Write a function to connect to Bedrock Foundation Model - Claude Foundation Model
+
 def utemia_llm():
     llm=Bedrock(
-        credentials_profile_name='default',
-        model_id='anthropic.claude-v2',
-        model_kwargs={
+    credentials_profile_name='default',
+    model_id='anthropic.claude-v2',
+    model_kwargs={
         "max_tokens_to_sample":3000,
-        "temperature": 0.9,
+        "temperature": 0.5,
         "top_p": 0.9})
     return llm
-#6b. Write a function which searches the user prompt, searches the best match from Vector DB and sends both to LLM.
+
 def utemia_rag_response(index,question):
     utemia_rag_llm=utemia_llm()
     utemia_rag_query=index.query(question=question,llm=utemia_rag_llm)
     return utemia_rag_query
-# Index creation --> https://api.python.langchain.com/en/latest/indexes/langchain.indexes.vectorstore.VectorstoreIndexCreator.html
--- utemia_frontend.py
+++ utemia_frontend.py
@@ -1,21 +1,20 @@
-# The below frontend code is provided by AWS and Streamlit. I have only modified it to make it look attractive.
 import streamlit as st 
-import utemia_backend as demo ### replace rag_backend with your backend filename
+import utemia_backend as demo 
 
-st.set_page_config(page_title="Reglamento estudiantil Q & A Prototipo-UtemIA-Chatbot") ### Modify Heading
+st.set_page_config(page_title="Prototipo-UtemIA-Chatbot") 
 
-new_title = '<p style="font-family:sans-serif; color:Green; font-size: 42px;">HR Q & A Prototipo-Utemsito-Chatbot 🎯</p>'
-st.markdown(new_title, unsafe_allow_html=True) ### Modify Title
+new_title = '<p style="font-family:sans-serif; color:Green; font-size: 42px;">Reglamento estudiantil RAG - Q & A Prototipo-UtemIA-Chatbot 🎯</p>'
+st.markdown(new_title, unsafe_allow_html=True) 
 
 if 'vector_index' not in st.session_state: 
-    with st.spinner("📀 Generando Vector DB...."): ###spinner message
-        st.session_state.vector_index = demo.utemia_index() ### Your Index Function name from Backend File
+    with st.spinner("📀 Generando prototipo...."): 
+        st.session_state.vector_index = demo.utemia_index() 
 
 input_text = st.text_area("Input text", label_visibility="collapsed") 
-go_button = st.button("Ingresar respuesta", type="primary") ### Button Name
+go_button = st.button("Ingresar respuesta", type="primary") 
 
 if go_button: 
     
-    with st.spinner("📢 Generando respuesta...."): ### Spinner message
+    with st.spinner("📢 Generando respuesta...."): 
         response_content = demo.utemia_rag_response(index=st.session_state.vector_index, question=input_text) ### replace with RAG Function from backend file
         st.write(response_content) 
