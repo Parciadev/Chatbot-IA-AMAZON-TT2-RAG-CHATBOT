557216c87962ffc884aceb205ec0011c3928190e
Your Name <142696188+PabloGarciaU@users.noreply.github.com>
2024-04-30 21:14:26 -0400

    nuevos cambios
    tratando de encontrar un mejor rendimiento en los rags

--- utemia_backend.py
+++ utemia_backend.py
@@ -6,22 +6,25 @@ from langchain_community.vectorstores import FAISS
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.llms.bedrock import Bedrock
 
+DATA_PATH = 'docs'
+
 def utemia_index():
-    docs_dir = 'docs'
     loaders = []
-
-    for file_name in os.listdir(docs_dir):
+    for file_name in os.listdir(DATA_PATH):
         if file_name.endswith('.pdf'):
-            file_path = os.path.join(docs_dir, file_name)
-            print(f"Cargando archivo: {file_path}")
+            file_path = os.path.join(DATA_PATH, file_name)
             loader = PyPDFLoader(file_path)
+            print(f"Cargando archivo: {file_path}")
             loaders.append(loader)
-
+    
     data_split = RecursiveCharacterTextSplitter( 
-        chunk_size=800,
-        chunk_overlap=100,
-        separators=["\n\n", "\n", "\n\n\n", "\n\n\n\n", "\n\n\n\n\n", "\n\n\n\n\n\n", "\n\n\n\n\n\n\n", "\n\n\n\n\n\n\n\n"]
-        )
+        chunk_size=1000,
+        chunk_overlap=500,
+        length_function=len,
+        add_start_index=True,
+    )
+
+    #chunks = data_split.split_documents(loaders)
     
     data_embeddings = BedrockEmbeddings(
         credentials_profile_name='default',
@@ -36,16 +39,30 @@ def utemia_index():
     return db_index
 
 def utemia_llm():
-    llm=Bedrock(
-    credentials_profile_name='default',
-    model_id='anthropic.claude-v2',
-    model_kwargs={
-        "max_tokens_to_sample":3000,
-        "temperature": 0.5,
-        "top_p": 0.9})
+    llm = Bedrock(
+        credentials_profile_name='default',
+        model_kwargs={
+            "max_tokens_to_sample": 1000,
+            "temperature": 0.9,
+            "top_k": 250,
+            "top_p": 0.9
+        },
+        model_id='anthropic.claude-v2'
+    )
+    
     return llm
 
-def utemia_rag_response(index,question):
-    utemia_rag_llm=utemia_llm()
-    utemia_rag_query=index.query(question=question,llm=utemia_rag_llm)
+def utemia_chatbot(bot_context=None):
+    initial_prompt = """¡Hola! Soy Utem-ia, un asistente de IA diseñado para ayudar a los estudiantes y académicos de la universidad   
+    con sus preguntas y tareas. Mi conocimiento proviene de un conjunto de documentos académicos cargados en mi base de datos. 
+    Puedo responder preguntas sobre una amplia variedad de temas relacionados con la universidad.
+    Por favor, siéntete libre de hacer cualquier pregunta o solicitar ayuda que necesites. Estoy aquí para ayudarte.
+
+    {bot_context if bot_context else ''}"""
+    return initial_prompt
+
+def utemia_rag_response(index, question):
+    utemia_rag_llm = utemia_llm()
+    inicial_prompt = utemia_chatbot()
+    utemia_rag_query = index.query(question=question, llm=utemia_rag_llm)
     return utemia_rag_query
--- utemia_frontend.py
+++ utemia_frontend.py
@@ -1,20 +1,22 @@
-import streamlit as st 
-import utemia_backend as demo 
+import streamlit as st
+import utemia_backend as demo
 
-st.set_page_config(page_title="Prototipo-UtemIA-Chatbot") 
+st.set_page_config(page_title="Prototipo-UtemIA-Chatbot")
 
-new_title = '<p style="font-family:sans-serif; color:Green; font-size: 42px;">Reglamento estudiantil RAG - Q & A Prototipo-UtemIA-Chatbot 🎯</p>'
-st.markdown(new_title, unsafe_allow_html=True) 
+logo_filename = "UTEM.png"
+st.image(logo_filename, width=300)
 
-if 'vector_index' not in st.session_state: 
-    with st.spinner("📀 Generando prototipo...."): 
-        st.session_state.vector_index = demo.utemia_index() 
+new_title = '<p style="font-family:sans-serif; color:Green; font-size: 42px;">Prototipo-UtemIA-Chatbot 🎯 RAG - Q & A </p>'
+st.markdown(new_title, unsafe_allow_html=True)
 
-input_text = st.text_area("Input text", label_visibility="collapsed") 
-go_button = st.button("Ingresar respuesta", type="primary") 
+if 'vector_index' not in st.session_state:
+    with st.spinner("📀 Generando prototipo...."):
+        st.session_state.vector_index = demo.utemia_index()
 
-if go_button: 
-    
-    with st.spinner("📢 Generando respuesta...."): 
-        response_content = demo.utemia_rag_response(index=st.session_state.vector_index, question=input_text) ### replace with RAG Function from backend file
-        st.write(response_content) 
+input_text = st.text_area("Chatea con UtemIA aquí...", label_visibility="collapsed")
+go_button = st.button("Enviar", key="response_button", help="Presiona para recibir una respuesta del chatbot")
+
+if go_button:
+    with st.spinner("📢 Generando respuesta...."):
+        response_content = demo.utemia_rag_response(index=st.session_state.vector_index, question=input_text)
+        st.write(response_content)
